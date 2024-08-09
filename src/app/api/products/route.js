ba3e561f6170f20263f30c10c13745993c311e2e
Javmir <167805975+javgamma@users.noreply.github.com>
2024-08-09 20:21:33 +0200

    Fixed MongoDB client import and usage

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -1,30 +1,30 @@
-import { NextResponse } from 'next/server';
-import { getClient } from '../../../../lib/mongodb.mjs';
+// import { getClient } from '../../../../lib/mongodb.mjs';
 import { v4 as uuidv4 } from 'uuid';
+import clientPromise from '../../../../lib/mongodb.mjs';
+import { NextResponse } from 'next/server';
 
 export async function GET() {
   try {
-    const client = await getClient();
+    const client = await clientPromise;
     const db = client.db("cafeDB");
     const products = await db.collection("products").find({}).toArray();
     return NextResponse.json(products);
-  } catch (e) {
-    return NextResponse.json({ error: 'Error fetching products' }, { status: 500 });
+  } catch (error) {
+    console.error('Error in GET function:', error);
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
   }
 }
 
 export async function POST(request) {
   try {
-    const client = await getClient();
+    const client = await clientPromise;
     const db = client.db("cafeDB");
-    const products = await request.json();
-    const productsToInsert = products.map(product => ({
-      ...product,
-      _id: uuidv4()
-    }));
-    const result = await db.collection("products").insertMany(productsToInsert);
-    return NextResponse.json(result, { status: 201 });
-  } catch (e) {
-    return NextResponse.json({ error: 'Error inserting products' }, { status: 500 });
+    const data = await request.json();
+    const newProduct = { ...data, _id: uuidv4() };
+    await db.collection("products").insertOne(newProduct);
+    return NextResponse.json(newProduct, { status: 201 });
+  } catch (error) {
+    console.error('Error in POST function:', error);
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
   }
 }
