963e8c856bf72abbbcd93983099475345b623aa4
Javmir <167805975+javgamma@users.noreply.github.com>
2024-08-13 01:19:12 +0200

    slice to array
    limits the array to only the first four elements of the database. Database is updated, now it is sorted according to figma guidelines.

--- src/app/(main)/NewsSection.jsx
+++ src/app/(main)/NewsSection.jsx
@@ -37,9 +37,9 @@ const NewsSection = () => {
 
   return (
     <div className="p-6">
-      <h1 className="text-4xl font-bold text-center mb-8">Our Coffee Products</h1>
+      <h1 className="text-4xl font-bold text-center mb-8">Novedades</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-        {Array.isArray(products) && products.map((product) => (
+        {Array.isArray(products) && products.slice(0,4).map((product) => (
           <div key={product.brand} className="bg-white shadow-md rounded-lg overflow-hidden hover:shadow-xl transition-shadow duration-300">
             <div className="relative w-full h-48">
               <Image
