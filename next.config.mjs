20d5c0e35f1749e084f1ecc8345f4bec566ae74e
Javmir <167805975+javgamma@users.noreply.github.com>
2024-08-12 18:59:49 +0200

    add hero to page
    Hero is added, details are still missing. Ideally, it should become a component (atom).

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,20 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-    reactStrictMode: true,
- 
-        images: {
-          remotePatterns: [
-            {
-              protocol: 'https',
-              hostname: 'res.cloudinary.com',
-              port: '',
-              pathname: '/**',
-            },
-          ],
-        },
-      
+  reactStrictMode: true,
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+    domains: ["https://cafe-de-alturanew.vercel.app/"], // Add this line
     
-    
-  }
+  },
+};
   
   export default nextConfig;
